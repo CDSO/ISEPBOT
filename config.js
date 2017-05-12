7674af5cac861508b1bc8fadd8a0aa8e9e17d106
Stephane Dennery <stephane.dennery@gmail.com>
2017-05-12 10:56:05 +0200

    Mise en place du bot Attila

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  token: 'MzEyNDg2Mzk0NzQ2MzcyMDk3.C_bxRg.irrQpWG2gvDWmGypbCfZ8klH1hc',
+  //Munchkin token: 'MzEyNDg2Mzk0NzQ2MzcyMDk3.C_bxRg.irrQpWG2gvDWmGypbCfZ8klH1hc',
+  token: 'MzEyNTA3Mjk0NDc2Nzk1OTA3.C_cE4A.H5qtGndY6NoPkL1fWjTNGOCVaiw',
   channel: 'channelid'
 }
