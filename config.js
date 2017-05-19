578265aec4934a2d65c7f56aa9b83dd6665dd63e
Stephane Dennery <stephane.dennery@gmail.com>
2017-05-19 11:12:19 +0200

    changement de Token(Attila)

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  Munchkin token: 'MzEyNDg2Mzk0NzQ2MzcyMDk3.C_bxRg.irrQpWG2gvDWmGypbCfZ8klH1hc',
-  //token: 'MzEyNTA3Mjk0NDc2Nzk1OTA3.C_cE4A.H5qtGndY6NoPkL1fWjTNGOCVaiw',
+ // Munchkin token: 'MzEyNDg2Mzk0NzQ2MzcyMDk3.C_bxRg.irrQpWG2gvDWmGypbCfZ8klH1hc',
+  token: 'MzEyNTA3Mjk0NDc2Nzk1OTA3.DABIAA.frZ9q2mX5yxb7CrCRwlsGuZJkJ4',
   channel: 'channelid'
 }
