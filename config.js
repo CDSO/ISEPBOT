203a0859845fdc3af317b3186b8fbc182f494663
Stephane Dennery <stephane.dennery@gmail.com>
2017-05-19 09:28:55 +0200

    petit commentaire

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  //Munchkin token: 'MzEyNDg2Mzk0NzQ2MzcyMDk3.C_bxRg.irrQpWG2gvDWmGypbCfZ8klH1hc',
-  
-  token: 'MzEyNTA3Mjk0NDc2Nzk1OTA3.C_cE4A.H5qtGndY6NoPkL1fWjTNGOCVaiw',
+  Munchkin token: 'MzEyNDg2Mzk0NzQ2MzcyMDk3.C_bxRg.irrQpWG2gvDWmGypbCfZ8klH1hc',
+  //token: 'MzEyNTA3Mjk0NDc2Nzk1OTA3.C_cE4A.H5qtGndY6NoPkL1fWjTNGOCVaiw',
   channel: 'channelid'
 }
