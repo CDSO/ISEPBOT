fec6320bcb276b1b35f7ef31e425c7c2ccda558a
Stephane Dennery <stephane.dennery@gmail.com>
2017-05-25 22:49:23 +0200

    API twitter (tweet)

--- bot.js
+++ bot.js
@@ -1,22 +1,7 @@
 const Discord = require('discord.js')
 var Twit = require('twit')
 const config = require('./config.js')
-var T = new Twit(config)
 const client = new Discord.Client()
-
-var tweet = {
-  status: 'Je vaincrai !!'
-}
-
-T.post('statuses/update', tweet, tweeted)
-
-function tweeted (err, data, response) {
-  if (err) {
-    console.log('Il y a une erreur')
-  } else {
-    console.log('Ca roule')
-  }
-}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
@@ -31,6 +16,20 @@ client.on('message', msg => {
   if (msg.content === 'hello') {
     msg.channel.sendMessage("Hello Warrior, Let's fight !")
   }
+
+  var caractere = msg.content
+  caractere = caractere.substring(7)
+
+  if (msg.content === '!tweet ' + caractere) {
+    var tweet = {
+      status: caractere
+    }
+    var T = new Twit(config)
+    T.post('statuses/update', tweet)
+    msg.channel.send("C'est bon, je tweet !")
+  }
 })
+
+
 
 client.login(config.token)
