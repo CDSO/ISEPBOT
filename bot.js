a91f752d35e163c680d776ae41cfafbba5df81da
20paka <pauline.creppy@gmail.com>
2017-06-10 23:32:29 +0200

    API youtube

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js')
 const config = require('./config.js')
 const client = new Discord.Client()
+var YouTube = require('youtube-node')
+var youTube = new YouTube()
+youTube.setKey('AIzaSyAGo3CB1AHk8okk32VDQ3F9qAyi47oMPq0')
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
@@ -9,12 +12,59 @@ client.on('ready', () => {
 client.on('message', msg => {
   // Check if the message has been posted in a channel where the bot operates
   // and that the author is not the bot itself
-  if (msg.channel.type !== 'dm' && (config.channel !== msg.channel.id || msg.author.id === client.user.id)) return
+  if (msg.channel.type !== 'dm' && (config.channel[msg.channel.id] !== msg.author.id === client.user.id)) return
 
   // If message is hello, post hello too
   if (msg.content === 'hello') {
-    msg.channel.sendMessage('Hello to you too, fellow !')
+    msg.channel.sendMessage('hello to you too')
+  }
+
+  var caractere = msg.content
+  caractere = caractere.substring(9)
+  if (msg.content === '!youtube ' + caractere) {
+    youTube.search(caractere, 3, function (error, result) {
+      if (error) {
+        console.log(error)
+      }
+
+      if (JSON.stringify(result.items[0].id.kind, null, 2) === '"youtube#playlist"') {
+        msg.channel.sendMessage('https://www.youtube.com/watch?v=' + JSON.stringify(result.items[0].snippet.thumbnails.default.url, null, 2).substring(24, 35) + '&list=' + JSON.stringify(result.items[0].id.playlistId, null, 2).substring(1, JSON.stringify(result.items[0].id.playlistId, null, 2).length - 1))
+      }
+
+      if (JSON.stringify(result.items[0].id.kind, null, 2) === '"youtube#channel"') {
+        msg.channel.sendMessage('https://www.youtube.com/channel/' + JSON.stringify(result.items[0].id.channelId, null, 2).substring(1, JSON.stringify(result.items[0].id.channelId, null, 2).length - 1))
+      }
+
+      if (JSON.stringify(result.items[0].id.kind, null, 2) === '"youtube#video"') {
+        msg.channel.sendMessage('https://www.youtube.com/watch?v=' + JSON.stringify(result.items[0].id.videoId, null, 2).substring(1, JSON.stringify(result.items[0].id.videoId, null, 2).length - 1))
+      }
+
+      if (JSON.stringify(result.items[1].id.kind, null, 2) === '"youtube#playlist"') {
+        msg.channel.sendMessage('https://www.youtube.com/watch?v=' + JSON.stringify(result.items[1].snippet.thumbnails.default.url, null, 2).substring(24, 35) + '&list=' + JSON.stringify(result.items[1].id.playlistId, null, 2).substring(1, JSON.stringify(result.items[1].id.playlistId, null, 2).length - 1))
+      }
+
+      if (JSON.stringify(result.items[1].id.kind, null, 2) === '"youtube#channel"') {
+        msg.channel.sendMessage('https://www.youtube.com/channel/' + JSON.stringify(result.items[1].id.channelId, null, 2).substring(1, JSON.stringify(result.items[1].id.channelId, null, 2).length - 1))
+      }
+
+      if (JSON.stringify(result.items[1].id.kind, null, 2) === '"youtube#video"') {
+        msg.channel.sendMessage('https://www.youtube.com/watch?v=' + JSON.stringify(result.items[1].id.videoId, null, 2).substring(1, JSON.stringify(result.items[1].id.videoId, null, 2).length - 1))
+      }
+
+      if (JSON.stringify(result.items[2].id.kind, null, 2) === '"youtube#playlist"') {
+        msg.channel.sendMessage('https://www.youtube.com/watch?v=' + JSON.stringify(result.items[2].snippet.thumbnails.default.url, null, 2).substring(24, 35) + '&list=' + JSON.stringify(result.items[2].id.playlistId, null, 2).substring(1, JSON.stringify(result.items[2].id.playlistId, null, 2).length - 1))
+      }
+
+      if (JSON.stringify(result.items[2].id.kind, null, 2) === '"youtube#channel"') {
+        msg.channel.sendMessage('https://www.youtube.com/channel/' + JSON.stringify(result.items[2].id.channelId, null, 2).substring(1, JSON.stringify(result.items[2].id.channelId, null, 2).length - 1))
+      }
+
+      if (JSON.stringify(result.items[2].id.kind, null, 2) === '"youtube#video"') {
+        msg.channel.sendMessage('https://www.youtube.com/watch?v=' + JSON.stringify(result.items[2].id.videoId, null, 2).substring(1, JSON.stringify(result.items[2].id.videoId, null, 2).length - 1))
+      }
+    })
   }
 })
 
 client.login(config.token)
+
--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
   token: 'MzEyNDg2Mzk0NzQ2MzcyMDk3.C_bxRg.irrQpWG2gvDWmGypbCfZ8klH1hc',
-  channel: 'channelid'
+  channel: 'channelid',
+  setkey: 'AIzaSyAGo3CB1AHk8okk32VDQ3F9qAyi47oMPq0'
 }
