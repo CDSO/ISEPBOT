b8c1629843480395abaa2e85a8ea3f1ee0976805
Benjamin <bsong95@gmmail.com>
2017-06-11 16:24:03 +0200

    Ajout du bot Spotify

--- bot.js
+++ bot.js
@@ -18,8 +18,8 @@ client.on('message', msg => {
   if (msg.content === 'Quoi') {
     msg.channel.send('Feur')
   }
-
-  var spotify = require('spotify')
+//  Old code for Spotify library
+/*  var spotify = require('spotify')
   if (msg.content.startsWith('!spotify')) {
     var args = msg.content.split(' ').slice(1)
     if (args[0] !== '!artist' && args[0] !== '!track' && args[0] !== '!album') {
@@ -34,20 +34,6 @@ client.on('message', msg => {
         msg.channel.send('__**Titre**__ : ' + data.tracks.items[0].name)
         msg.channel.send(data.tracks.items[0].external_urls.spotify)
       })
- //     spotify.search({ type: 'album', query: args.join(' ') }, function (err, data) {
- //       if (err) {
- //         msg.channel.send('Il y eu une erreur')
- //       }
- //       msg.channel.send('__**Album**__ : ' + data.albums.items[0].name)
- //       msg.channel.send(data.albums.items[0].external_urls.spotify)
- //     })
- //     spotify.search({ type: 'track', query: args.join(' ') }, function (err, data) {
- //       if (err) {
- //         msg.channel.send('Il y eu une erreur')
- //       }
- //       msg.channel.send('__**Titre**__ : ' + data.tracks.items[0].name)
- //       msg.channel.send(data.tracks.items[0].external_urls.spotify)
- //     })
     }
     if (args[0] === '!artist') {
       spotify.search({ type: 'artist', query: args.join(' ').substring(7) }, function (err, data) {
@@ -76,6 +62,63 @@ client.on('message', msg => {
         msg.channel.send(data.tracks.items[0].external_urls.spotify)
       })
     }
+  } */
+
+  if (msg.content.startsWith('!spotify')) {
+    // Create the api object with the credentials
+    var SpotifyWebApi = require('spotify-web-api-node')
+    var spotifyApi = new SpotifyWebApi({
+      clientId: '4d3c749c8112468fbeeee15c53a59135',
+      clientSecret: '935e7bf613e34383b562994f27102128'
+    })
+    var args = msg.content.split(' ').slice(1)
+    // Retrieve an access token.
+    spotifyApi.clientCredentialsGrant()
+    .then(function (data) {
+      spotifyApi.setAccessToken(data.body['access_token'])
+      if (args[0] !== '!artist' && args[0] !== '!track' && args[0] !== '!album') {
+        spotifyApi.search(args.join(' '), ['artist', 'album', 'track'], {limit: 1}, function (err, data) {
+          if (err) {
+            console.error('Something wrong', err)
+          }
+          msg.channel.send('__**Artiste**__ : ' + data.body.artists.items[0].name)
+          msg.channel.send(data.body.artists.items[0].external_urls.spotify)
+          msg.channel.send('__**Album**__ : ' + data.body.albums.items[0].name)
+          msg.channel.send(data.body.albums.items[0].external_urls.spotify)
+          msg.channel.send('__**Titre**__ : ' + data.body.tracks.items[0].name)
+          msg.channel.send(data.body.tracks.items[0].external_urls.spotify)
+        })
+      }
+      if (args[0] === '!artist') {
+        spotifyApi.searchArtists(args.join(' ').substring(7), {limit: 1}, function (err, data) {
+          if (err) {
+            console.error('Something wrong', err)
+          }
+          msg.channel.send('__**Artiste**__ : ' + data.body.artists.items[0].name)
+          msg.channel.send(data.body.artists.items[0].external_urls.spotify)
+        })
+      }
+      if (args[0] === '!album') {
+        spotifyApi.searchAlbums(args.join(' ').substring(6), {limit: 1}, function (err, data) {
+          if (err) {
+            console.error('Something wrong', err)
+          }
+          msg.channel.send('__**Album**__ : ' + data.body.albums.items[0].name)
+          msg.channel.send(data.body.albums.items[0].external_urls.spotify)
+        })
+      }
+      if (args[0] === '!track') {
+        spotifyApi.searchTracks(args.join(' ').substring(6), {limit: 1}, function (err, data) {
+          if (err) {
+            console.error('Something wrong', err)
+          }
+          msg.channel.send('__**Titre**__ : ' + data.body.tracks.items[0].name)
+          msg.channel.send(data.body.tracks.items[0].external_urls.spotify)
+        })
+      }
+    }, function (err) {
+      console.log('Something went wrong when retrieving an access token', err)
+    })
   }
 })
 
