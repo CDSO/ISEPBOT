ec721a12be5ac417344e9284a41486544c7c10b8
Stephane Dennery <stephane.dennery@gmail.com>
2017-06-11 18:19:57 +0200

    changement de token

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
- // Munchkin token: 'MzEyNDg2Mzk0NzQ2MzcyMDk3.C_bxRg.irrQpWG2gvDWmGypbCfZ8klH1hc',
-  token: 'MzEyNTA3Mjk0NDc2Nzk1OTA3.DABIAA.frZ9q2mX5yxb7CrCRwlsGuZJkJ4',
+  token: 'MzEyNDg2Mzk0NzQ2MzcyMDk3.C_bxRg.irrQpWG2gvDWmGypbCfZ8klH1hc',
   channel: 'channelid',
   consumer_key: 'YqG4yCkwOQjTR8yNoecSgtBvd',
   consumer_secret: 'KvMzgyn9TOEERg9zsaO1QL3RphrjLGBCRSL3RDV56zXtiRZveq',
