45d35645ee4327fa8f78a2f9d886ca220de8343d
ISEPAlex <alex.obertelli@gmail.com>
2017-06-12 19:45:06 +0200

    config file was corrupted

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 module.exports = {
+
   token: 'MzEyNDg2Mzk0NzQ2MzcyMDk3.C_bxRg.irrQpWG2gvDWmGypbCfZ8klH1hc',
   channel: 'channelid',
   consumer_key: 'YqG4yCkwOQjTR8yNoecSgtBvd',
   consumer_secret: 'KvMzgyn9TOEERg9zsaO1QL3RphrjLGBCRSL3RDV56zXtiRZveq',
   access_token: '865496788190412803-ZOQKLaTMXlpMRn9whagwMHfKe4eaOjd',
-  access_token_secret: 'gtrF78hBD8lWMszSvMCdpwd1W9E7AIlTufXdeZbYB2QH8'
+  access_token_secret: 'gtrF78hBD8lWMszSvMCdpwd1W9E7AIlTufXdeZbYB2QH8',
   setkey: 'AIzaSyAGo3CB1AHk8okk32VDQ3F9qAyi47oMPq0'
 }
