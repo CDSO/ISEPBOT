76d70f0caacbfd1ecc51050a6183af47b7975c36
ISEPAlex <alex.obertelli@gmail.com>
2017-06-12 17:02:24 +0200

    correction pokemon

--- bot.js
+++ bot.js
@@ -2,19 +2,17 @@ const Discord = require('discord.js')
 var Twit = require('twit')
 const config = require('./config.js')
 
-
 var T = new Twit(config)
 
-const Translate = require('@google-cloud/translate')
 const pokemonClient = require('pokeapi')
 var pokeAPI = pokemonClient.v1()
-
+var pokemonToBe = ''
+var trueBotName = ''
 
 const client = new Discord.Client()
-const projectId = 'Google ChatBot'
-var pokemonToBe = ''
 
-// Instantiates a client
+const Translate = require('@google-cloud/translate')
+const projectId = 'Google ChatBot'
 const translateClient = Translate({
   projectId: projectId
 })
@@ -22,9 +20,8 @@ const translateClient = Translate({
 client.on('ready', () => {
   console.log('Logged in as ' + client.user.username + '!')
   pokemonToBe = client.user.username
+  trueBotName = pokemonToBe
 })
-
-
 
 client.on('message', msg => {
   // Check if the message has been posted in a channel where the bot operates
@@ -61,7 +58,6 @@ client.on('message', msg => {
   })
 })
 
-// bot Translate et bot Pokemon (quelques soucis avec le bot Pokemon)
 client.on('message', (message) => {
   var author = message.author
   var messageArray = message.content.split(' ')
@@ -85,59 +81,66 @@ client.on('message', (message) => {
         })
   } else if (messageArray[0] === '!pokemon') {
     if (messageArray[1] === 'evolve') {
-      pokeAPI.get({ resource_uri: '/api/v2/pokemon-species/' + client.user.username.toLowerCase() + '/' }).then(function (evolve) {
+      pokeAPI.get({ resource_uri: '/api/v2/pokemon-species/' + trueBotName.toLowerCase() + '/' }).then(function (evolve) {
         var chainEvolution = evolve.evolution_chain.url.split('/')
-        console.log(chainEvolution)
         pokeAPI.get({ resource_uri: '/api/v2/evolution-chain/' + chainEvolution[chainEvolution.length - 2] + '/' }).then(function (pokemonEvolutionChain) {
           var evolutionChain = pokemonEvolutionChain.chain
           var pokemonNameTest = evolutionChain.species.name
-          while (pokemonNameTest !== client.user.username.toLowerCase()) {
-            console.log(pokemonNameTest)
+          while (pokemonNameTest !== trueBotName.toLowerCase()) {
             evolutionChain = evolutionChain.evolves_to[0]
             pokemonNameTest = evolutionChain.species.name
-            console.log(pokemonNameTest)
           }
-          pokemonToBe = evolutionChain.evolves_to[0].species.name
+          if (evolutionChain.evolves_to[0] !== null) {
+            pokemonToBe = evolutionChain.evolves_to[0].species.name
+            trueBotName = pokemonToBe
+          } else {
+            pokemonToBe = trueBotName
+          }
+          setPokemon(pokemonToBe, message)
         })
       })
     } else {
       pokemonToBe = messageArray[1]
+      trueBotName = pokemonToBe
+      setPokemon(pokemonToBe, message)
     }
-    console.log(pokemonToBe)
-    pokeAPI.get({ resource_uri: '/api/v2/pokemon/' + pokemonToBe + '/' }).then(function (pokemon) {
-      var nationalID = pokemon.id
-      var pokemonName = pokemon.name
-      var typesList = pokemon.types
-      var typesListLength = typesList[0].slot
-      var typesFinal = ''
-      var pokemonHeight = pokemon.height
-      var pokemonWeight = pokemon.weight
-      var pokemonSprites = pokemon.sprites.front_default
-
-      client.user.setAvatar(pokemonSprites)
-      client.user.setUsername(pokemonName)
-
-      if (typesListLength === 1) {
-        typesFinal = 'My type is ' + typesList[0].type.name + '.\n'
-      } else if (typesListLength > 1) {
-        typesFinal = 'My types are '
-        var j = 0
-        for (j = 0; j < typesListLength; j++) {
-          typesFinal = typesFinal + typesList[j].type.name
-          if (j < typesListLength - 1) {
-            typesFinal = typesFinal + ' and '
-          }
-        }
-        typesFinal = typesFinal + '.\n'
-      } else {
-        typesFinal = "I don't have any type. \n"
-      }
-
-      var finalMessage = "Hello, I'm " + pokemonName + '. In the pokedex, you will find me at id ' + nationalID + '.\n' + typesFinal + 'I weigth ' + pokemonWeight / 10 + ' kg and measure ' + pokemonHeight / 10 + ' m.'
-
-      message.channel.send(finalMessage)
-    })
   }
 })
+
+function setPokemon (pokemonToBe, message) {
+  pokeAPI.get({ resource_uri: '/api/v2/pokemon/' + pokemonToBe + '/' }).then(function (pokemon) {
+    var nationalID = pokemon.id
+    var pokemonName = pokemon.name
+    var typesList = pokemon.types
+    var typesListLength = typesList[0].slot
+    var typesFinal = ''
+    var pokemonHeight = pokemon.height
+    var pokemonWeight = pokemon.weight
+    var pokemonSprites = pokemon.sprites.front_default
+
+    client.user.setAvatar(pokemonSprites)
+    client.user.setUsername(pokemonName)
+
+    if (typesListLength === 1) {
+      typesFinal = 'My type is ' + typesList[0].type.name + '.\n'
+    } else if (typesListLength > 1) {
+      typesFinal = 'My types are '
+      var j = 0
+      for (j = 0; j < typesListLength; j++) {
+        typesFinal = typesFinal + typesList[j].type.name
+        if (j < typesListLength - 1) {
+          typesFinal = typesFinal + ' and '
+        }
+      }
+      typesFinal = typesFinal + '.\n'
+    } else {
+      typesFinal = "I don't have any type. \n"
+    }
+
+    var finalMessage = "Hello, I'm " + pokemonName + '. In the pokedex, you will find me at id ' + nationalID + '.\n' + typesFinal + 'I weigth ' + pokemonWeight / 10 + ' kg and measure ' + pokemonHeight / 10 + ' m.'
+
+    message.channel.send(finalMessage)
+  })
+}
 
 client.login(config.token)
